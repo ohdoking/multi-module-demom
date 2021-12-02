69092a99d36339ac94342895032475c1d590ef44
Dokeun Oh <dokeun.oh@car-ewallet.de>
2021-12-02 08:10:49 +0100

    first commit

---
+++ src/test/java/com/ohdoking/multidemo/MultiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohdoking.multidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
